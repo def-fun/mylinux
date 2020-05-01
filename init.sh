45302e98a6c2f978fa4dec9626b099b1a67b1ac8
admin <goojayfan@qq.com>
2020-05-01 20:22:34 +0800

    添加一些工具

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 # install tools that used frequently
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install nano bash-completion silversearcher-ag
+sudo apt install nano bash-completion silversearcher-ag htop p7zip-full zip unzip unrar-free sreen lrzsz \
+    nload python3-bs4  python3-pymongo python3-dateutil
